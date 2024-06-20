35357dc509fb0c93610bacfcc11234bebf7fd1c5
Codemason43 <145558386+Israel-Arendse@users.noreply.github.com>
2024-06-20 11:53:13 +0400

    Update auth_users.js (Added registered users login)
    In this updated code I am using the imported "jsonwebtoken" module, to create JWTs (line 2).  In the '/login' route in line 17,  the code checks if a user with the provided username and password exists in the users' array (lines 18, 19, and 21). 
    
    If a user exists, a JWT is created with the username as the payload and "access" as the secret key (lines 23, 24, and 25). The created JWT is then saved in the session and is sent in the response (line 26).
    
    If the user does not exist, a response with the status 401 and the message "Invalid username or password" is sent (line 28). 
    
    Finally, the app.use function mounts the 'reged_users' router at the /customer/login path. This means the '/login' route will be accessible in the '/customer/login' endpoint (line 33).

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -13,11 +13,24 @@ const authenticatedUser = (username,password)=>{ //returns boolean
 //write code to check if username and password match the one we have in records.
 }
 
-//only registered users can login
+//Only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;  
+  const password = req.body.password;
+
+  const user = users.find(user => user.username === username && user.password === password); //Checks if the username and password currently exist in the array
+
+  if(user){
+    const accessToken = jwt.sign({username: user.username}, "access"); 
+    req.session.authorization = {accessToken};
+    return res.status(200).json({message: "User successfully logged in", accessToken}); // Sends a response if the user is successfully logged in
+  } else {
+    return res.status(401).json({message: "Invalid username or password"}); // Sends a response if the user has an invalid username or password
+  }
+
 });
+
+app.user("/customer/login", regd_users); //Sets the endpoint for login to "/customer/login"
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
