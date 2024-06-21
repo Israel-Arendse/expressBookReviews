7611084c0ee38fc5c79d9b604a92e4fd0ad8b283
Codemason43 <145558386+Israel-Arendse@users.noreply.github.com>
2024-06-21 12:14:12 +0400

    Update auth_users.js (Added authenticatedUser and IsValid function)
    I have created two functions to check if the logged-in username and password are valid, the authenticatedUser and isValid function.
    
     So when the user logs in, the authenticatedUser function is checks if the username and password are the same ones in the users' array.
    
    The IsVaild function is used to validate that the username is at least 6 characters long and alphanumeric.

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -3,14 +3,20 @@ const jwt = require('jsonwebtoken');
 let books = require("./booksdb.js");
 const regd_users = express.Router();
 
-let users = [];
+let users = [
+  { 
+    "username":"Twain71",  
+    "password": "one2free4",
+  }
+];
 
-const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+const isValid = (username)=>{ //returns boolean to show if username is valid or not.
+  return username.length >= 6 && /^[a-z0-9]+$/i.test(username);
 }
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+const authenticatedUser = (username,password)=>{
+   const user = users.find(user => user.username === username && user.password === password);
+   return user !== undefined;
 }
 
 //Only registered users can login
@@ -20,7 +26,7 @@ regd_users.post("/login", (req,res) => {
 
   const user = users.find(user => user.username === username && user.password === password); //Checks if the username and password currently exist in the array
 
-  if(user){
+  if(authenticatedUser(username, password)){
     const accessToken = jwt.sign({username: user.username}, "access"); 
     req.session.authorization = {accessToken};
     return res.status(200).json({message: "User successfully logged in"}); // Sends a response if the user is successfully logged in
