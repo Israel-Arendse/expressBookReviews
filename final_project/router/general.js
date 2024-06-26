5f78ebc9754b999a01191a3befed0b1417903853
Codemason43 <145558386+Israel-Arendse@users.noreply.github.com>
2024-06-26 18:22:32 +0400

    Update general.js ( Created a promise for book details based on ISBN )
    The code for fetching the book details based on the ISBN is now based on a promise that is resolved when the book is found or rejected if the book does not exists.

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -59,20 +59,32 @@ public_users.get("/", async function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn', function (req, res) {
-  const isbn = req.params.isbn;
-
-  // Convert the books object to an array of book objects
-  const booksArray = Object.values(books);
-
-  // Find the book with the matching ISBN
-  const book = booksArray.find(book => book.isbn === isbn);
-  
-  if (book) {
-    res.send(book);
-  } else {
-    res.send({ message:"Book does not exist"});
-  }
-});
+   const isbn = req.params.isbn;
+   console.log(`Getting book details based on the ISBN: ${isbn}...`);
+ 
+   // Convert the books object to an array of book objects
+   const booksArray = Object.values(books);
+ 
+   // Wrap the logic inside a new Promise
+   new Promise((resolve, reject) => {
+     // Find the book with the matching ISBN
+     const book = booksArray.find(book => book.isbn === isbn);
+ 
+     if (book) {
+       console.log(`Found book details based on the ISBN: ${isbn}.`);
+       resolve(book);
+     } else {
+       console.log(` The Book with the selected ISBN:${isbn} does not exist.`)
+       reject(new Error("Book does not exist"));
+     }
+   })
+   .then(book => {
+     res.send(book);
+   })
+   .catch(error => {
+     res.send({ message: error.message });
+   });
+ });
 
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
